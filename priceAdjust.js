fea205ac174b8cc9d54b8019b26d83c6b6373b47
broette <77510420+broette@users.noreply.github.com>
2021-03-15 19:39:16 -0400

    updated product page to be dynamic

---
+++ priceAdjust.js
@@ -0,0 +1,21 @@
+$(function() {
+  var price = parseFloat($('#price').text());
+
+  $('#subtract').on("click",function() {
+    var $qty = $('#valuebox');
+    var current = parseInt($qty.val());
+    if ( current > 0 ) {
+        $qty.val(current-1);
+        $('#total').val(price*(current-1));
+    } else {
+        $('#total').val(0);
+    }
+  });
+
+  $('#add').on("click",function() {
+    var $qty = $('#valuebox');
+    var current = parseInt($qty.val());
+    $qty.val(current+1);
+    $('#total').val(price*(current+1));
+  });
+});
